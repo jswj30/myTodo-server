1e28cd57398051db9abdb506fcf5e4bf74d90a75
jswj30 <jswj30@naver.com>
2021-05-18 18:08:48 +0900

    [feat21][feature]signin 수정 1

--- controller/user/signin.js
+++ controller/user/signin.js
@@ -6,29 +6,18 @@ module.exports = {
     try {
       // req에서 email, password 저장하기
       const { email, password } = req.body;
-
-      // 정보가 덜 왔을 경우
-      if (!email || !password) {
-        res.status(422).send("모든 정보를 입력해주세요.");
-      }
-
-      // user 찾기
-      const result = await user.findOne({
-        where: { email, password },
+      // 세션 regenerate
+      req.session.regenerate(async (err) => {
+        // user 찾기
+        const result = await user.findOne({
+          where: { email, password },
+        });
+        if (result) {
+          res.status(200).send("success!");
+        } else {
+          res.status(404).send("유저를 찾을 수 없습니다.");
+        }
       });
-
-      if (result) {
-        res
-          .status(200)
-          .json({
-            id: result.id,
-            email: result.email,
-            name: result.name,
-            mobile: result.mobile,
-          });
-      } else {
-        res.status(404).send("유저를 찾을 수 없습니다.");
-      }
     } catch (error) {
       res.status(500).send(error);
     }
