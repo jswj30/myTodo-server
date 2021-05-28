8c1eb568ea50a02d6c39250e03c07eddb5b148d6
jswj30 <jswj30@naver.com>
2021-05-28 15:46:27 +0900

    [feat41][feature]중요 체크한 todolist 출력 API 생성

--- controller/user/important.js
+++ controller/user/important.js
@@ -1,6 +1,45 @@
-const { status } = require("../../models");
+const { todo, status } = require("../../models");
 
 module.exports = {
+  get: async (req, res) => {
+    const sess = req.session;
+    try {
+      if (sess.userid) {
+        let todos = await todo.findAll({
+          where: {
+            userId: sess.userid,
+          },
+          attributes: ["id", "content", "startDate"],
+          include: {
+            model: status,
+            where: {
+              important: true,
+            },
+            attributes: ["important", "complete"],
+          },
+        });
+
+        if (todos) {
+          let todolist = todos.map((a) => {
+            return {
+              id: a.id,
+              content: a.content,
+              startDate: a.startDate,
+              important: a.statuses[0].important,
+              complete: a.statuses[0].complete,
+            };
+          });
+          res.status(200).json(todolist);
+        } else {
+          res.sendStatus(204);
+        }
+      } else {
+        res.status(401).send("Not found Session!");
+      }
+    } catch (err) {
+      res.status(404).send(err);
+    }
+  },
   patch: async (req, res) => {
     try {
       const { id } = req.body;
