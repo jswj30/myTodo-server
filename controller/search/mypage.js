26b7322416dec749be913d415e2f734b315e0299
jswj30 <jswj30@naver.com>
2021-05-29 11:59:27 +0900

    [feat43][feature]유저 정보 출력 API 생성

---
+++ controller/search/mypage.js
@@ -0,0 +1,39 @@
+const { user } = require("../../models");
+
+module.exports = {
+  // 유저 정보 확인
+  post: async (req, res) => {
+    const sess = req.session;
+    try {
+      if (sess.userid) {
+        const { id, password } = req.body;
+        let userInfo = await user.findOne({
+          where: {
+            id,
+            password,
+          },
+        });
+
+        if (userInfo) {
+          const { email, password, name, mobile } = userInfo;
+          let hidePassword = password
+            .split("")
+            .reduce((acc, val) => (acc += "*"), "");
+          let result = {
+            email,
+            password: hidePassword,
+            name,
+            mobile,
+          };
+          res.status(200).send(result);
+        } else {
+          res.status(404).send("유저를 찾을 수 없습니다.");
+        }
+      } else {
+        res.status(401).send("Not found Session!");
+      }
+    } catch (err) {
+      res.status(404).send(err);
+    }
+  },
+};
